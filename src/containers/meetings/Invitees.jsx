c56075a512ec9aabc131f03cd5e42deb246e59aa
Sahil-Arifi <sahil.arifi2006@gmail.com>
2020-09-07 18:02:13 -0700

    started work on invitees avatar

--- src/containers/meetings/Invitees.jsx
+++ src/containers/meetings/Invitees.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+
+import Avatar from "../../components/avatar/Avatar";
 
 const Invitees = ({ invitees }) => {
   return (
     <div>
-      {invitees && invitees.map(invitee => {
-        return <div key={invitee.inviteeId}>{invitee.name}</div>
-      })}
+      {invitees &&
+        invitees.map((invitee) => {
+          return (
+            <Avatar
+              key={invitee.inviteeId}
+              type="circle"
+              img={invitee.photoUrl}
+              size="30px"
+            />
+          );
+        })}
     </div>
-  )
-}
+  );
+};
 
 Invitees.propTypes = {
-  invitees: PropTypes.array
-}
+  invitees: PropTypes.array,
+};
 
-export default Invitees
+export default Invitees;
