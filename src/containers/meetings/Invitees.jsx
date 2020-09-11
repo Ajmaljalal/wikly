aac0f7f2abc4d70024e88cf04e9bd78b66d6bf2c
Sahil-Arifi <sahil.arifi2006@gmail.com>
2020-09-11 08:45:06 -0700

    Added avatar icons that are apart of a meeting. Also added if there is more than 7 people put the extra in one icon that shows how many more people there are.

--- src/containers/meetings/Invitees.jsx
+++ src/containers/meetings/Invitees.jsx
@@ -3,22 +3,45 @@ import PropTypes from "prop-types";
 
 import Avatar from "../../components/avatar/Avatar";
 
+import { InviteeStyles } from "./assets/styles/meetingInvitees.styles";
+
 const Invitees = ({ invitees }) => {
+  const imagesToShow = 7;
   return (
-    <div>
+    <InviteeStyles.Container>
       {invitees &&
-        invitees.map((invitee) => {
-          return (
-            <Avatar
-              key={invitee.inviteeId}
-              type="circle"
-              img={invitee.photoUrl}
-              size="30px"
-            />
-          );
+        invitees.map((invitee, index) => {
+          if (index >= imagesToShow) {
+            return null;
+          } else {
+            return (
+              <InviteeStyles.Avatar>
+                <Avatar
+                  key={invitee.inviteeId}
+                  type="circle"
+                  img={invitee.photoUrl}
+                  size="30px"
+                  initials={invitee.initials}
+                />
+              </InviteeStyles.Avatar>
+            );
+          }
         })}
-    </div>
+      {remainingInviteesNumber(invitees, imagesToShow)}
+    </InviteeStyles.Container>
   );
+};
+
+const remainingInviteesNumber = (invitees, imagesToShow) => {
+  if (invitees && invitees.length >= imagesToShow) {
+    return (
+      <InviteeStyles.Avatar style={{ background: "green" }}>
+        +{invitees.length - imagesToShow}
+      </InviteeStyles.Avatar>
+    );
+  } else {
+    return null;
+  }
 };
 
 Invitees.propTypes = {
