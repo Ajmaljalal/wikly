aad1586b76d3929899496ae2857995aa8ccdb646
Sahil-Arifi <sahil.arifi2006@gmail.com>
2020-09-12 14:06:31 -0700

    Added tooltips when hovered over avatar icons, and added plus icon to add new invitees.

--- src/containers/meetings/assets/styles/meetingInvitees.styles.jsx
+++ src/containers/meetings/assets/styles/meetingInvitees.styles.jsx
@@ -2,16 +2,17 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  margin-top: 10px;
-  margin-left: 12px;
+  align-items: center;
+  margin-top: 12px;
+  margin-left: 10px;
 `;
 
 const Avatar = styled.div`
   position: relative;
-  border: 2px solid #fff;
+  border: 2.5px solid #fff;
   border-radius: 50%;
-  width: 33px;
-  height: 33px;
+  width: 31px;
+  height: 31px;
   margin-left: -12px;
   display: flex;
   justify-content: center;
@@ -19,7 +20,24 @@ const Avatar = styled.div`
   color: white;
 `;
 
+const AddIcon = styled.div`
+  transition: 0.5s background;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 33px;
+  height: 33px;
+  box-sizing: border-box;
+  cursor: pointer;
+  &:hover {
+    background: dodgerblue;
+    color: white;
+  }
+`;
+
 export const InviteeStyles = {
   Avatar,
   Container,
+  AddIcon,
 };
