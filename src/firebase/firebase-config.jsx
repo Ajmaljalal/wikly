38fc966833d4bd01f9397eb8a85b7e681d9c701f
Ajmal Jalal <jalalajmal96@gmail.com>
2023-02-09 18:31:17 -0800

    Update firebase-config.jsx

--- src/firebase/firebase-config.jsx
+++ src/firebase/firebase-config.jsx
@@ -3,14 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 export const firebaseConfigs = {
-    apiKey: "AIzaSyAr6QuHTkatVZ9dPfj9YbXBSGD1XTqUFGE",
-    authDomain: "wikli-dev.firebaseapp.com",
-    databaseURL: "https://wikli-dev.firebaseio.com",
-    projectId: "wikli-dev",
-    storageBucket: "wikli-dev.appspot.com",
-    messagingSenderId: "489721199821",
-    appId: "1:489721199821:web:b4af86a9d631a30bfe3d84",
-    measurementId: "G-E422SW37NZ"
+
 };
   
 if (!firebase.apps.length) {
